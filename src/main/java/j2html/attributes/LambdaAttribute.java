1789ba02b28ea3797bfa2ca6d36a5f324377eec2
David <davidaase@hotmail.com>
2018-01-08 21:14:51 +0100

    Add parameter name reflection

---
+++ src/main/java/j2html/attributes/LambdaAttribute.java
@@ -0,0 +1,26 @@
+package j2html.attributes;
+
+// Written by Benjamin Weber (http://benjiweber.co.uk/blog/author/benji/)
+
+import j2html.reflection.MethodFinder;
+import java.util.function.Function;
+
+public interface LambdaAttribute extends MethodFinder, Function<String, Object> {
+
+    default String name() {
+        checkParametersEnabled();
+        return parameter(0).getName();
+    }
+
+    default String value() {
+        checkParametersEnabled();
+        return String.valueOf(this.apply(name()));
+    }
+
+    default void checkParametersEnabled() {
+        if ("arg0".equals(parameter(0).getName())) {
+            throw new IllegalStateException("You also need java 8u60 or newer for parameter reflection to work");
+        }
+    }
+
+}
